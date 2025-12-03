56f49067509ec12c4367357ac5f6b8b3803e580f
11-Sarthak <your@email.com>
2025-12-04 02:11:30 +0530

    add to system files

--- app/application.py
+++ app/application.py
@@ -37,7 +37,10 @@ app = FastAPI(
 # ----------------- CORS -----------------
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Streamlit, localhost, Render
+    allow_origins=[
+        "http://localhost:8501",  # Local Streamlit during development
+        "https://socialmediaapplicationinfastapibysarthak.streamlit.app",  # Hosted Streamlit frontend
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
